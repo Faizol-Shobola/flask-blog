9bf5444767cc9329d9a455ff0b7a82cf25aa6956
Shobola Faizol <shobolafaizol@gmail.com>
2022-12-04 12:07:17 +0100

    Backup files

---
+++ flaskblog/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '0ba5c68cde41c994600db54b953593aa'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Kayfz@localhost:5432/blogapp'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flaskblog import routes
